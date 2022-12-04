dfe75f51063f590852125a4db6cb52e81eb048c4
Gantcho Kojuharov <gantcho@Gantchos-Air.fritz.box>
2022-12-04 13:43:39 +0100

    initial

---
+++ src/routes/+page.server.js
@@ -0,0 +1,29 @@
+import { SECRET_RAWG_API_KEY } from '$env/static/private';
+
+const rnd = (max) => Math.floor(Math.random() * max);
+
+export const load = async ({ fetch }) => {
+	const baseUrl = 'https://api.rawg.io/api';
+	const gamesUrl = `${baseUrl}/games?key=${SECRET_RAWG_API_KEY}`;
+    const ordering = ["name", "released", "added", "created", "updated", "rating", "metacritic"]
+
+	const randomPage = rnd(1335) + 1;
+    const gameIndex = rnd(5);
+    const ord = rnd(ordering.length);
+
+	const games = await fetch(`${gamesUrl}&page_size=5&page=${randomPage}&metacritic=1,100&ordering=${ordering[ord]}`);
+    const allGames = (await games.json()).results;
+	const game = allGames[gameIndex];
+	const snapshots = await fetch(
+		`${baseUrl}/games/${game.id}/screenshots?key=${SECRET_RAWG_API_KEY}`
+	)
+    const allSnapshots = (await snapshots.json()).results;
+
+    console.log(`Answer is ${game.name}, num of snapshot: ${allSnapshots.length}`)
+
+	return {
+        answer: game.name,
+		gameNames: allGames.map((gm) => gm.name),
+		snapshot: allSnapshots[rnd(allSnapshots.length)].image
+	};
+};
---
+++ src/routes/+page.svelte
@@ -0,0 +1,62 @@
+<script>
+	export let data;
+
+	function guess() {
+		console.log(this.textContent);
+		if (this.textContent === data.answer) {
+			this.classList.add('right');
+		} else {
+			this.classList.add('wrong');
+		}
+	}
+</script>
+
+<div class="container">
+	<img class="snapshot" src={data.snapshot} alt="Game pic" />
+
+	<div class="answers">
+		{#each data.gameNames as name}
+			<button on:click={guess} class:wrong={false} class:right={false}>{name}</button>
+		{/each}
+	</div>
+</div>
+
+<style>
+    :global(body) {
+        background-color: darkslategrey;
+    }
+
+	div.container {
+		margin: 1em;
+		display: grid;
+		grid-template-columns: auto auto;
+        gap: 1em;
+	}
+
+	div.answers {
+		grid-column-start: 2;
+		grid-column-end: 2;
+	}
+
+	img.snapshot {
+		grid-column-start: 1;
+		grid-column-end: 1;
+        width: 100%
+	}
+
+	button {
+		display: block;
+		padding: 0.5em;
+		margin-bottom: 0.3em;
+		font-size: large;
+		width: 100%;
+	}
+
+	button.wrong {
+		background-color: red;
+	}
+
+	button.right {
+		background-color: green;
+	}
+</style>
