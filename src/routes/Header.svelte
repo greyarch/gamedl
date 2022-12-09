1f844f6ab2ec85930abc0ad461084d26ac6cb8f1
Gantcho Kojuharov <gantcho@greyarch.net>
2022-12-09 22:31:15 +0100

    improved responsiveness

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -5,31 +5,14 @@
 <header>
 	<nav class="shadow">
 		<div class="flex justify-between items-center py-2 px-10 container mx-auto">
-			<div>
-				<h1
-					class="text-2xl font-bold bg-gradient-to-tr from-indigo-600 to-green-600 bg-clip-text text-transparent hover:cursor-pointer"
-				>
-					Guess the Game!
-				</h1>
-			</div>
-			<div>
-				<div class="hover:cursor-pointer sm:hidden">
-					<spnan
-						class="h-1 rounded-full block w-8 mb-1 bg-gradient-to-tr from-indigo-600 to-green-600"
-					/>
-					<spnan
-						class="h-1 rounded-full block w-8 mb-1 bg-gradient-to-tr from-indigo-600 to-green-600"
-					/>
-					<spnan
-						class="h-1 rounded-full block w-8 mb-1 bg-gradient-to-tr from-indigo-600 to-green-600"
-					/>
-				</div>
-				<div class="flex items-center">
-					<h1 class="sm:flex text-xl space-x-4 hidden items-center">
-						Your streak: {$current} / {$best}
-					</h1>
-				</div>
-			</div>
+			<h1
+				class="text-2xl font-bold bg-gradient-to-tr from-indigo-600 to-green-600 bg-clip-text text-transparent hover:cursor-pointer"
+			>
+				Guess the Game!
+			</h1>
+			<h1 class="flex items-center text-xl">
+				Your streak: {$current} / {$best}
+			</h1>
 		</div>
 	</nav>
 </header>
