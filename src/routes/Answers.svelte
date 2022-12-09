1e6c10932e88e93bacb52603cabaaff92e9cf670
Gantcho Kojuharov <gantcho@greyarch.net>
2022-12-09 15:52:09 +0100

    buttons entirely clickable

--- src/routes/Answers.svelte
+++ src/routes/Answers.svelte
@@ -1,18 +1,17 @@
 <script>
 	import { createEventDispatcher } from 'svelte';
-	
-    export let answer;
+
+	export let answer;
 	export let gameNames;
 
 	const dispatch = createEventDispatcher();
 
 	function guess() {
-        parent = this.parentElement;
 		if (this.textContent === answer) {
-			parent.classList.add('bg-green-500', 'text-white');
-            dispatch("guessed")
+			this.classList.add('bg-green-500', 'text-white');
+			dispatch('guessed');
 		} else {
-			parent.classList.add('bg-red-500', 'text-white');
+			this.classList.add('bg-red-500', 'text-white');
 		}
 	}
 </script>
@@ -20,12 +19,10 @@
 <div class="bg-white">
 	<div class="list-none text-center">
 		{#each gameNames as name}
-			<li class="py-3 hover:border-gray-700 border-2">
-				<button
-					on:click={guess}
-					class="list-none">{name}</button
-				>
-			</li>
+			<button
+				on:click={guess}
+				class="list-none py-3 hover:border-gray-700 border-2 w-[100%] h-[100%]">{name}</button
+			>
 		{/each}
 	</div>
 </div>
--- src/routes/PlayAgain.svelte
+++ src/routes/PlayAgain.svelte
@@ -1,5 +1,5 @@
 <div class="bg-blue-500 text-white list-none text-center">
-	<li class="py-3 border-b-5">
-		<button on:click={() => location.reload(true)}>WELL DONE! PLAY AGAIN?</button>
-	</li>
+	<button on:click={() => location.reload(true)} class="py-3 border-b-5 w-[100%] h-[100%]"
+		>WELL DONE! PLAY AGAIN?</button
+	>
 </div>
