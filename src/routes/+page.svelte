a12deaf61cbd61700ac73de2ee16081f8f6f18c1
Gantcho Kojuharov <gantcho@greyarch.net>
2023-10-21 14:21:09 +0200

    side right ad unit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -28,6 +28,24 @@
 					<hr />
 					<PlayAgain />
 				{/if}
+				<div class="mb-2" />
+				<script
+					async
+					src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-0312355607953387"
+					crossorigin="anonymous"
+				></script>
+				<!-- side-right -->
+				<ins
+					class="adsbygoogle"
+					style="display:block"
+					data-ad-client="ca-pub-0312355607953387"
+					data-ad-slot="2289431070"
+					data-ad-format="auto"
+					data-full-width-responsive="true"
+				/>
+				<script>
+					(adsbygoogle = window.adsbygoogle || []).push({});
+				</script>
 			</div>
 		</div>
 	</section>
