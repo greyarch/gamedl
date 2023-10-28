86653b9c3c3df6f64485a1c3699a73b6ed65a439
Gantcho Kojuharov <gantcho@greyarch.net>
2023-10-28 16:04:25 +0200

    async reaload of images

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -5,23 +5,26 @@ const rnd = (max) => Math.floor(Math.random() * max);
 export const load = async ({ fetch }) => {
 	const baseUrl = 'https://api.rawg.io/api';
 	const gamesUrl = `${baseUrl}/games?key=${SECRET_RAWG_API_KEY}`;
-    const ordering = ["name", "released", "added", "created", "updated", "rating", "metacritic"]
+	const ordering = ['name', 'released', 'added', 'created', 'updated', 'rating', 'metacritic'];
 
 	const randomPage = rnd(1335) + 1;
-    const gameIndex = rnd(5);
-    const ord = rnd(ordering.length);
+	const gameIndex = rnd(5);
+	const ord = rnd(ordering.length);
 
-	const games = await fetch(`${gamesUrl}&page_size=5&page=${randomPage}&metacritic=1,100&ordering=${ordering[ord]}`);
-    const allGames = (await games.json()).results;
+	const games = await fetch(
+		`${gamesUrl}&page_size=5&page=${randomPage}&metacritic=1,100&ordering=${ordering[ord]}`
+	);
+	const allGames = (await games.json()).results;
 	const game = allGames[gameIndex];
-	const snapshots = await fetch(
-		`${baseUrl}/games/${game.id}/screenshots?key=${SECRET_RAWG_API_KEY}`
-	)
-    const allSnapshots = (await snapshots.json()).results;
+	const snapshot = fetch(`${baseUrl}/games/${game.id}/screenshots?key=${SECRET_RAWG_API_KEY}`)
+		.then((allSnapshots) => allSnapshots.json())
+		.then((s) => s.results[rnd(s.count)].image);
 
 	return {
-        answer: game.name,
+		answer: game.name,
 		gameNames: allGames.map((gm) => gm.name),
-		snapshot: allSnapshots[rnd(allSnapshots.length)].image
+		streamed: {
+			snapshot
+		}
 	};
 };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,7 +16,12 @@
 		<div
 			class="bg-gray-200 sm:grid grid-cols-4 grid-rows-1 px-4 py-6 min-h-full lg:min-h-screen space-y-6 sm:space-y-0 sm:gap-4"
 		>
-			<Snapshot image={data.snapshot} />
+			{#await data.streamed.snapshot}
+				<div class="col-span-3">Loading...</div>
+			{:then snapshot}
+				<Snapshot image={snapshot} />
+			{/await}
+
 			<div class="h-96 col-span-1">
 				<Answers
 					on:guessed={() => (showPlayAgain = true)}
@@ -29,23 +34,6 @@
 					<PlayAgain />
 				{/if}
 				<div class="mb-2" />
-				<script
-					async
-					src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-0312355607953387"
-					crossorigin="anonymous"
-				></script>
-				<!-- side-right -->
-				<ins
-					class="adsbygoogle"
-					style="display:block"
-					data-ad-client="ca-pub-0312355607953387"
-					data-ad-slot="2289431070"
-					data-ad-format="auto"
-					data-full-width-responsive="true"
-				/>
-				<script>
-					(adsbygoogle = window.adsbygoogle || []).push({});
-				</script>
 			</div>
 		</div>
 	</section>
